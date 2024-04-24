bd465564821afa1a180c3c99188657f4704174fb
Bernard Leckning <bernard.leckning@menzies.edu.au>
2024-04-25 01:06:51 +1000

    Updated color scheme

--- app.R
+++ app.R
@@ -2,6 +2,9 @@ library(shiny)
 library(bslib)
 library(LexisPlotR)
 library(tidyverse)
+library(colorspace)
+
+s19 <- sequential_hcl(19, palette = "BluYl")
 
 ui <- page_sidebar(
   title = "CYDRP retrospective cohort study design tool",
@@ -42,19 +45,19 @@ server <- function(input, output) {
     if(input$peri) {
       peri_start <- paste(input$year[1], "-01-01", sep = "")
       peri_end <- paste(min(2017, input$year[2]), "-01-01", sep = "")
-      l <- lexis_polygon(lg = l, x = c(peri_start, peri_start, peri_end, peri_end), y = c(0, 1, 1, 0), fill = "#0E3F5C")
+      l <- lexis_polygon(lg = l, x = c(peri_start, peri_start, peri_end, peri_end), y = c(0, 1, 1, 0), fill = s19[12])
     }
     if(input$ia) {
-      l <- lexis_year(lg = l, fill = "#144D65", max(input$year[1], 2000), 2019-max(input$year[1], 2000))
+      l <- lexis_year(lg = l, fill = s19[2], max(input$year[1], 2000), 2019-max(input$year[1], 2000))
     }
     if(input$poli) {
       poli_start <- paste(max(2014, input$year[1]), "-01-01", sep = "")
       poli_end <- paste(input$year[2], "-01-01", sep = "")
-      l <- lexis_polygon(lg = l, x = c(poli_start, poli_start, poli_end, poli_end), y = c(10, input$age[2], input$age[2], 10), fill = "#1D5B6E")
+      l <- lexis_polygon(lg = l, x = c(poli_start, poli_start, poli_end, poli_end), y = c(10, input$age[2], input$age[2], 10), fill = s19[4])
     }
     cohort_start <- input$cohort[1] + input$age[1]
     l <- lexis_cohort(lg = l, cohort = cohort_start, delta = input$cohort[2]-input$cohort[1], fill = "orange")
-    l <- l + geom_hline(yintercept=0, color="green")
+    l <- l + geom_hline(yintercept=0, color="red")
     l
   })
 }
